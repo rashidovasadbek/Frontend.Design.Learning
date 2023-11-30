060051cdf35af2b7451c323ed889256dc51e3e83
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-30 22:20:15 +0500

    Update json files

--- src/N78-AirBnB-css/tailwind.config.js
+++ src/N78-AirBnB-css/tailwind.config.js
@@ -4,13 +4,20 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '545px',
+      md: '960px',
+      lg: '1280px',
+    },
     extend: {
       colors: {
         textPrimary: '#222222',
         textSecondary: '#818181',
         logoSelected: '#000000',
         logoSecondary: '#717171',
-        logoPrimary: '#ff385c'
+        logoAccent: '#dddddd',
+        logoPrimary: '#ff385c',
+        borderSecondary: '#cccccc',
       }
     },
   },
