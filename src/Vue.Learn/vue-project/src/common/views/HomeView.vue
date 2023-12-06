11caa998f714b1bca0cd7fcfdc4a556911e8221a
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-06 22:31:00 +0500

    Added AirBnB companentes

---
+++ src/Vue.Learn/vue-project/src/common/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <article class="mt-[20px] px[24px] px-[80px]">
+
+    <div class="flex flex-col items-center justify-center">
+
+      <locations-grid/>
+
+    </div>
+
+  </article>
+
+</template>
+<script setup lang="ts">
+import LocationsGrid from "@/modules/locations/companents/LocationsGrid.vue";
+</script>
